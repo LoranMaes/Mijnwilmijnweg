86fbd7371162d156e93af6b73526938825f0c06e
Loran Maes <loran.maes@student.odisee.be>
2022-04-01 01:41:39 +0200

    alles, behalve fully responsive

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 (function(){
     "use strict"
     const hamburger = document.querySelector("#hamburger")
-
+    console.log(hamburger)
     hamburger.addEventListener("click", function(){
         if(hamburger.classList.contains("active")){
             hamburger.classList.remove("active")
